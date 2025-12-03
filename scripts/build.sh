21d47ed8a6276834503a9b69e6fd15667c481a14
Nick Fedor <nick@nickfedor.com>
2025-12-03 12:25:13 -0700

    chore: update makefile and build script (#501)
    - Change build target to use build.sh script
    - Update docs targets to use virtual environment and mkdocs
    - Modify build output path to ./bin/shoutrrr

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ COMMIT=$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
 DATE=$(git log -1 --format=%cd --date=iso | date -u +%Y-%m-%dT%H:%M:%SZ 2>/dev/null || echo "unknown")
 
 # Build with ldflags
-go build -ldflags "-s -w -X github.com/nicholas-fedor/shoutrrr/internal/meta.Version=$VERSION -X github.com/nicholas-fedor/shoutrrr/internal/meta.Commit=$COMMIT -X github.com/nicholas-fedor/shoutrrr/internal/meta.Date=$DATE" -o shoutrrr ./shoutrrr
+go build -ldflags "-s -w -X github.com/nicholas-fedor/shoutrrr/internal/meta.Version=$VERSION -X github.com/nicholas-fedor/shoutrrr/internal/meta.Commit=$COMMIT -X github.com/nicholas-fedor/shoutrrr/internal/meta.Date=$DATE" -o ./bin/shoutrrr ./shoutrrr
