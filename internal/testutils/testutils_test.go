f24a65d472faa99e593aa25aece23ca6f82d31f2
Nick Fedor <nick@nickfedor.com>
2026-09-06 18:11:01 -0700

    test(testutils): drop unused dummyService fields
    - Remove Logger and Templater from dummyService setup

--- internal/testutils/testutils_test.go
+++ internal/testutils/testutils_test.go
@@ -112,8 +112,6 @@ var _ = ginkgo.Describe("the testutils package", func() {
 
 		ginkgo.BeforeEach(func() {
 			service = dummyService{
-				Logger:    standard.Logger{},
-				Templater: standard.Templater{},
 				Config: dummyConfig{
 					EnumlessConfig: standard.EnumlessConfig{},
 					Foo:            0,
